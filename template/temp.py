23a2ae7a313387ca362b3f90255a27e84b9a2e0d
Ky-AnT <87473241+Ky-AnT@users.noreply.github.com>
2022-11-05 21:58:54 +0800

    Update temp.py

--- template/temp.py
+++ template/temp.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.ttk import Notebook
+import tkinter.font
 import sv_ttk
 app = Tk()
 app.geometry('300x10')
@@ -13,13 +14,14 @@ def initialize(subject,data):
     notebook.pack(pady=10, expand=True)
     frame1 = Frame(notebook, width=400, height=280)
     frame2 = Frame(notebook, width=400, height=280)
-    frame1.pack(fill='both', expand=True,)
+    frame1.pack(fill='both', expand=True)
     frame2.pack(fill='both', expand=True)
     notebook.add(frame1, text=subject)
     notebook.add(frame2, text='Account')
     notes = Text(frame1, height = 250, width = 395, bg = "white", relief='solid', borderwidth=0, highlightthickness=0) 
+    arial = tkinter.font.Font(family="Helvetica", size=13)
     notes.insert('0.0', data)
-    notes.config(padx=15, pady=10)
+    notes.config(padx=15, pady=10, font=arial)
     notes.pack()
     accountFile = open('data/account_data/account.txt','r').readlines()
     accountString = ""
